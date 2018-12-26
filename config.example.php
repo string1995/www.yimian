e8e90d98019f226d84740f3561fb8a35b101a4e4
string1995 <mail@liu.yimian.xyz>
2018-12-27 01:17:37 +0800

    config example

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+//This is the php config file
+$g_db_serverName="Your database server IP or Name";
+$g_db_usrName="Your database user name";
+$g_db_psswd="Your password";
+$g_db_dbName="The main database you select";
