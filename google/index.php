6df9eee0f4a541fb5012f716ad47c8a3a5570206
hhCandy <i@hhcandy.me>
2019-02-10 15:56:05 +0800

    update fp

--- google/index.php
+++ google/index.php
@@ -6,7 +6,7 @@
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE); 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, TRUE); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, TRUE);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     echo curl_exec($ch);
 	curl_close($ch);
